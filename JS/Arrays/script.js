976d0270cdc510113943f2c0eb701cb03661bd76
Irnity <77077359+irnity@users.noreply.github.com>
2021-08-07 11:43:17 +0300

    07.08

---
+++ JS/Arrays/script.js
@@ -0,0 +1,12 @@
+// push - add to end
+// pop - remove from end
+// shift - remove from start
+// unshift - add to start
+// const array3 = array1.concat(array2); (1+2)
+// array.includes('word')
+// array.indexOf('word')
+// array.reverse()
+// array.slice(2) all after third one
+// array.slice(3, 5) only third and fifth
+// array.splice(1, 0, "word") cuts, insert
+// array.sort()
---
+++ JS/Running JS Script/script.js
@@ -0,0 +1,52 @@
+// const password = prompt("please enter a new password")
+
+// if (password.length >= 6) {
+//   if (password.indexOf(" ") === -1) {
+//     console.log("Good job!")
+//   } else {
+//     console.log("password cannot contain spaces!")
+//   }
+// } else {
+//   console.log("Password too short! must be 6+")
+// }
+
+// const password = prompt("please enter your password")
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("valid")
+// } else {
+//   console.log("wrong")
+// }
+
+// const age = 90
+// if (age < 5 || age >= 65) {
+//   console.log("Free")
+// } else if (age < 10) {
+//   console.log("$10")
+// } else if (age < 65) {
+//   console.log("$20")
+// }
+
+// let firstName = prompt("enter name")
+// if (!firstName) {
+//   fistName = prompt("Try again!")
+// }
+
+//switch
+const day = 3
+switch (day) {
+  case 1:
+    console.log("monday")
+    break
+  case 2:
+    console.log("tuesday")
+    break
+  case 3:
+    console.log("wednesday")
+    break
+  case 4:
+    console.log("thursday")
+    break
+  default:
+    console.log("another day")
+    break
+}
